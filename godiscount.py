5c9b98307778a13048144be24ec0445a04f6511b
kitten1156 <frank1156kimo@gmail.com>
2021-08-14 05:41:24 +0800

    add pchome search & change pchome

--- godiscount.py
+++ godiscount.py
@@ -7,6 +7,7 @@ import json
 import sys,traceback
 import discountlib as dlib
 r = RethinkDB()
+sysstarttime = datetime.now()
 with r.connect( "localhost", 28015) as conn: #google excel處理
 	if r.now().day().run(conn) != r.db("line_notify_funny").table("option").get("intervalrenew").run(conn)["data"]: #每天把通知次數歸零
 		r.db("line_notify_funny").table("data").insert(r.db("line_notify_funny").table("data").without("intervalarr"),conflict="replace").run(conn)
@@ -28,8 +29,9 @@ with r.connect( "localhost", 28015) as conn: #google excel處理
 		if lineid == "":
 			index += 1
 			continue
-		if lineid in exceptid:
+		if lineid in exceptid: #清除無用的id
 			print("pass",lineid)
+			r.db("line_notify_funny").table("data").get(lineid).delete().run(conn)
 			index += 1
 			continue
 		print("處理",lineid)
@@ -57,7 +59,9 @@ with r.connect( "localhost", 28015) as conn: #google excel處理
 						if len(temp) >= 2:
 							try:
 								if temp[0] == "searchmomo":
-									searchproduct.append(["searchmomo",temp[1]," ".join(temp[2:])])
+									searchproduct.append(["searchmomo"," ".join(temp[1:])])
+								elif temp[0] == "searchpchome":
+									searchproduct.append(["searchpchome"," ".join(temp[1:])])
 								else:
 									price = int(temp[0])
 									url = temp[1]
@@ -66,8 +70,9 @@ with r.connect( "localhost", 28015) as conn: #google excel處理
 											url = url.split("?")[0]
 										if "momoshop.com.tw" in url:
 											momotop = url.index("i_code")
-											momolast = url.index("&",momotop)
-											url = url[:momolast]
+											momolast = url.find("&",momotop)
+											if momolast != -1:
+												url = url[:momolast]
 										if "https://momo.dm/" in url:
 											url = requests.get(url,headers=dlib.headers).url #縮寫型先轉換
 										product.append([price,url," ".join(temp[2:])])
@@ -94,20 +99,20 @@ with r.connect( "localhost", 28015) as conn: #google excel處理
 			if datetime.strptime(insertdata["starttime"],"%p %I:%M:%S") > datetime.strptime(insertdata["endtime"],"%p %I:%M:%S"):
 				dlib.sendtoline(lineid,"您填寫的時間有問題，請檢查資料，謝謝")
 				writetodb = False
-			if len(product) == 0:
+			if len(product)+len(searchproduct) == 0:
 				res = r.db("line_notify_funny").table("data").get(lineid).delete().run(conn)
 				if res["deleted"] == 1:
 					dlib.sendtoline(lineid,"資料清空")
 				writetodb = False
-			if len(product) >= 20:
+			if len(product)+len(searchproduct) >= 20:
 				dlib.sendtoline(lineid,"資料太多了，請減少到20筆")
 				writetodb = False
 			if writetodb:
 				resdb = r.db("line_notify_funny").table("data").insert(insertdata,conflict="update").run(conn)
 				if resdb["inserted"] == 1:
-					dlib.sendtoline(lineid,"資料新增成功")
+					dlib.sendtoline(lineid,"資料新增成功\r\n監控產品數："+str(len(product))+"\r\n監控搜尋數:"+str(len(searchproduct)))
 				if resdb["replaced"] == 1:
-					dlib.sendtoline(lineid,"資料修改成功")
+					dlib.sendtoline(lineid,"資料修改成功\r\n監控產品數："+str(len(product))+"\r\n監控搜尋數:"+str(len(searchproduct)))
 		except Exception as ex:
 			if "list index out of range" != str(ex):
 				print(ex)
@@ -117,37 +122,67 @@ with r.connect( "localhost", 28015) as conn: #google excel處理
 
 with r.connect( "localhost", 28015) as conn: #爬蟲gogo
 	print("do parser")
-	allproduct = {}
 	searchproductmomo = {}
+	searchproductpchome = {}
+	pchomeprod = {}
+	momoproduct = {}
 	for i in r.db("line_notify_funny").table("data").run(conn):
 		for j in i["product"]:
-			if j[1] not in allproduct.keys():
-				allproduct[j[1]]=[]
 			temp = i.copy()
 			temp["product"] = j
-			allproduct[j[1]].append(temp)
+			if "pchome.com.tw" in j[1]:
+				if j[1] not in pchomeprod.keys():
+					pchomeprod[j[1]]=[]
+				pchomeprod[j[1]].append(temp)
+			if "momoshop.com.tw" in j[1]:
+				if j[1] not in momoproduct.keys():
+					momoproduct[j[1]]=[]
+				momoproduct[j[1]].append(temp)
 		for j in i["searchproduct"]:
-			if j[1] not in searchproductmomo.keys():
+			if j[1] not in searchproductmomo.keys() and j[0] == "searchmomo":
 				searchproductmomo[j[1]]=[]
+			if j[1] not in searchproductpchome.keys() and j[0] == "searchpchome":
+				searchproductpchome[j[1]] = []
 			temp = i.copy()
-			searchproductmomo[j[1]].append(temp)
-	print("allproduct length:",len(allproduct))
-	for i in allproduct:
+			if j[0] == "searchmomo":
+				searchproductmomo[j[1]].append(temp)
+			if j[0] == "searchpchome":
+				searchproductpchome[j[1]].append(temp)
+	#處理pchome
+	print("pchomeprod length:",len(pchomeprod))
+	result = dlib.pchomemulti(pchomeprod.keys())
+	for i in pchomeprod:
+		try:
+			print(i.split("?")[0].split("/")[-1],end=" ")
+			js = result.get(i.split("?")[0].split("/")[-1]+"-000")
+			if js == None or js.get("Qty") == 0:
+				print("未販售")
+				continue
+			money = result[i.split("?")[0].split("/")[-1]+"-000"]["Price"]["P"]
+			print(js,money,end=" ")
+			print("")
+			for data in pchomeprod[i]: #取出有監控這個產品的人做比較
+				if data["product"][0] > money:
+					if data.get("intervalarr") == None:
+						data["intervalarr"] = {}
+					if datetime.strptime(data["starttime"],"%p %I:%M:%S").time() < datetime.now().time() and datetime.now().time() < datetime.strptime(data["endtime"],"%p %I:%M:%S").time():
+						sendtext = "現在價格:"+str(money)+" \r\n已達到設定的條件："+" ".join(str(i) for i in data["product"])
+						data["intervalarr"] = dlib.intervalcheck(data,i,data["id"],sendtext)
+						r.db("line_notify_funny").table("data").get(data["id"]).update({"intervalarr":data["intervalarr"]}).run(conn)
+		except Exception as ex:
+			print("pachome parser:",ex)
+			traceback.print_tb(sys.exc_info()[2])
+			pass
+	#處理momo
+	print("momoproduct length:",len(momoproduct))
+	for i in momoproduct:
 		try:
 			print(i,end=" ")
-			money = None
-			if "pchome.com.tw" in i:
-				money =  dlib.pchome(i)
-			if "momoshop.com.tw" in i:
-				money = dlib.momo(i)
-			if money == None:
-				print("have error")
-				continue
+			money = dlib.momo(i)
 			if money == "未販售":
 				print("未販售")
 				continue
-			alldata = allproduct[i]
-			for data in alldata:
+			for data in momoproduct[i]:  #取出有監控這個產品的人做比較
 				if data["product"][0] > money:
 					if data.get("intervalarr") == None:
 						data["intervalarr"] = {}
@@ -157,7 +192,7 @@ with r.connect( "localhost", 28015) as conn: #爬蟲gogo
 						r.db("line_notify_funny").table("data").get(data["id"]).update({"intervalarr":data["intervalarr"]}).run(conn)
 			print("")
 		except Exception as ex:
-			print("parser:",ex)
+			print("momo parser:",ex)
 			traceback.print_tb(sys.exc_info()[2])
 			pass
 	print("searchproductmomo length:",len(searchproductmomo))
@@ -178,8 +213,7 @@ with r.connect( "localhost", 28015) as conn: #爬蟲gogo
 					else:
 						sendmsg += " https://m.momoshop.com.tw/"+result[diff]["href"] +"\r\n"
 				if len(diffset) != 0:
-					alldata = searchproductmomo[i]
-					for data in alldata:
+					for data in searchproductmomo[i]:
 						if data.get("intervalarr") == None:
 							data["intervalarr"] = {}
 						if datetime.strptime(data["starttime"],"%p %I:%M:%S").time() < datetime.now().time() and datetime.now().time() < datetime.strptime(data["endtime"],"%p %I:%M:%S").time():
@@ -190,4 +224,35 @@ with r.connect( "localhost", 28015) as conn: #爬蟲gogo
 			print("search parser:",ex)
 			traceback.print_tb(sys.exc_info()[2])
 			pass
+	print("searchproductpchome length:",len(searchproductpchome))
+	for i in searchproductpchome:
+		try:
+			print(i,end=" ")
+			result = dlib.pchomeseatch(i)
+			dbresult = r.db("line_notify_funny").table("searchdata").get(i).run(conn)
+			if dbresult == None: #第一次建立資料 不發通知
+				r.db("line_notify_funny").table("searchdata").insert({"id":i,"data":result}).run(conn)
+			else:
+				diffset = set(list(result.keys())) - set(list(dbresult["data"].keys()))
+				sendmsg = ""
+				for diff in diffset:
+					sendmsg += "新增項目："+result[diff]["name"] + "\r\n 網址: https://24h.pchome.com.tw/prod/"+diff +"\r\n"
+				if len(diffset) != 0:
+					for data in searchproductpchome[i]:
+						if data.get("intervalarr") == None:
+							data["intervalarr"] = {}
+						if datetime.strptime(data["starttime"],"%p %I:%M:%S").time() < datetime.now().time() and datetime.now().time() < datetime.strptime(data["endtime"],"%p %I:%M:%S").time():
+							data["intervalarr"] = dlib.intervalcheck(data,i,data["id"],sendmsg)
+							r.db("line_notify_funny").table("data").get(data["id"]).update({"intervalarr":data["intervalarr"]}).run(conn)
+					r.db("line_notify_funny").table("searchdata").get(i).update({"data":result}).run(conn)
+		except Exception as ex:
+			print("search parser:",ex)
+			traceback.print_tb(sys.exc_info()[2])
+			pass
+	#清除沒有需要搜尋的key
+	print("clean search data")
+	key = list(r.db("line_notify_funny").table("searchdata").get_field("id").run(conn))-searchproductmomo.keys()-searchproductpchome.keys()
+	for i in key:
+		print(i,r.db("line_notify_funny").table("searchdata").get(i).delete().run(conn))
 	print("finish")
+print("time:",(datetime.now()-sysstarttime).total_seconds(),"秒")
